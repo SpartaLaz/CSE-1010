e9e65035b277c917f78632d576f604faf4848114
Alexg8212 <alexg01202806@gmail.com>
2025-11-17 19:46:34 -0500

    ADDED SCREEN 3 AND 4 ADDED BACKEND AND LOOP SCREEN 3 AND 4

--- project9_GUI.py
+++ project9_GUI.py
@@ -4,7 +4,7 @@ window = tk.Tk()
 window.title("Budget Buddy")
 window.geometry("840x500")
 window.configure(bg="lightblue")
-
+expense_obj = ""
 
 #screen 2
 def window2(name):
@@ -24,7 +24,7 @@ def window2(name):
                 income_label.config(text="Please enter a valid number!", fg="red")
                 return
         print("PLACEHOLDER INCOME IS", income)
-
+        window_expense_type()
 
     income_button = tk.Button(window, text="Submit", bg="white", fg="black", command=income_submit)
 
@@ -34,7 +34,67 @@ def window2(name):
     income_entry.pack()
     income_button.pack()
 
+#screen 3
+def window_expense_type():
+    #gets rid of all previous widgets without needing to call every singe one
+    for widget in window.winfo_children():
+        widget.destroy()
 
+    label = tk.Label(window, text="Enter an expense type that you want to add:", font=("Arial", 32, "bold"), bg="lightblue", fg="black")
+    status_label = tk.Label(window, text="", font=("Arial", 28, "bold"), bg="lightblue", fg="black")
+    type_entry = tk.Entry(window, width=50, bg="white", fg="black")
+
+    def submit_type():
+        global expense_obj
+        type = type_entry.get().strip()
+
+        if type == "":
+            status_label.config(text="Entry type cannot be empty!", fg="red")
+            return
+    # store in the backend budget() method **delete this comment once this is finished please**
+        expense_obj = type
+            
+        print("STORED EXPENSE TYPE =", type)
+
+        #window 4
+        window_expenses_nametype()
+
+    button = tk.Button(window, text="Submit", command=submit_type)
+    label.pack(pady=40)
+    status_label.pack()
+    type_entry.pack()
+    button.pack()
+
+def window_expenses_nametype():
+    #lets the user enter as many items until they click the finish button
+    for widget in window.winfo_children():
+        widget.destroy()
+    label = tk.Label(window, text=f"Adding expenses for {expense_obj}:", font=("Arial", 32, "bold"), bg="lightblue", fg="black")
+    instruction = tk.Label(window, text="Add expenses in 'type price' format (e.g Milk 20)", font=("Arial", 28, "bold"), bg="lightblue", fg="black")
+    status_label = tk.Label(window, text="", font=("Arial", 28, "bold"), bg="lightblue", fg="black")
+    entry = tk.Entry(window, width=50, bg="white", fg="black")  
+
+    def add():
+        text = entry.get().strip()
+        try:
+            category, price = text.split()
+            float(price)
+        except:
+            status_label.config(text="ENTER IN (TYPE COST) FORMAT", fg="red")
+            return
+        #add the stuff to the back end method add_expenses() **please delete after doing so**
+        print("EXPENSES ADDED", category, price)
+        entry.delete(0, "end")
+
+    add_btn = tk.Button(window, text="Add Expense", command=add)
+
+    label.pack(pady=30)
+    instruction.pack()
+    status_label.pack()
+    entry.pack()
+    add_btn.pack(padx=10)
+
+    
 
 welcome_label = tk.Label(window, text="Welcome to Budget Buddy!", font=("Arial", 40, "bold"), bg="lightblue", fg="black")
 
@@ -67,4 +127,6 @@ submit_button.pack()
 
 
 window.mainloop()
+
+
 
